dc13dca456e6cdd541bfb898f337ef3355a9483b
kimduongtuan2002 <kimduongtuan2002@gmail.com>
2025-10-20 18:40:05 +0700

    version 1.0.1

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("Hello John")
+const express = require("express"); //take library
+const app = express(); //create object
+const port = 3000;
+
+//request(call back) - response(handler)
+app.get("/", (req, res) => {
+    res.send("Hello duong tuan")
+})
+
+app.get("/about", (req, res) => {
+    res.send("This is page about")
+})
+
+
+app.listen(3000, () => {
+    console.log(`my app is running on port: ${port}`)
+})
+
