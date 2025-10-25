aabd00f5f32e69763c24779490bd1378270f0628
kimduongtuan2002 <kimduongtuan2002@gmail.com>
2025-10-25 17:48:36 +0700

    version 1.0.9

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,5 @@
 import e, { Request, Response } from "express";
-import { getAllUsers, handleCreateUser } from "../services/user-create";
+import { getAllUsers, handleCreateUser, handleDeleteUser } from "services/user-create";
 import { name } from "ejs";
 
 const getHomePage = async (rep: Request, res: Response) => { 
@@ -14,13 +14,21 @@ const getCreateUserPage = (rep: Request, res: Response) => {
    return res.render("create-user.ejs")
 };
 
-const postCreateUser = (req: Request, res: Response) => {
+const postCreateUser = async (req: Request, res: Response) => {
    const { fullName, email, address } = req.body; //get data objects
    
    // Handle create user
-   handleCreateUser(fullName, email, address);
+   await handleCreateUser(fullName, email, address);
 
-   return res.redirect("/")
+   return res.redirect("/");
 };
 
-export { getHomePage, getCreateUserPage, postCreateUser };
+const postDeleteUser = async (req: Request, res: Response) => {
+   const { id } = req.params; //get id
+   
+   await handleDeleteUser(id);
+
+   return res.redirect("/");
+};
+
+export { getHomePage, getCreateUserPage, postCreateUser, postDeleteUser };
--- src/services/user-create.ts
+++ src/services/user-create.ts
@@ -1,9 +1,22 @@
-import getConnection from "../config/database";
+import getConnection from "config/database";
 
 
-const handleCreateUser = (fullName: String, email: String, address: String) => {
+const handleCreateUser = async(fullName: String, email: String, address: String) => {
 
     // insert data to database
+    const connection = await getConnection();
+    try {
+        // query
+        const sql = 'INSERT INTO `users`(`name`, `email`, `address`) VALUES (?, ?, ?)';
+        const values = [fullName, email, address];
+
+        const [result, fields] = await connection.execute(sql, values);
+             
+    } catch (err) {
+        console.log(err);
+        return [];
+    }
+
 
     // return result
     console.log(">>> Insert data complete a new user");
@@ -23,4 +36,21 @@ const getAllUsers = async() => {
     }
 };
 
-export { handleCreateUser, getAllUsers };
+const handleDeleteUser = async(id: string) => {
+    try {
+    const connection = await getConnection();
+    const sql = 'DELETE FROM `users` WHERE `id` = ? LIMIT 1';
+    const values = [id];
+
+    const [result, fields] = await connection.execute(sql, values);
+    
+    return result;
+    } catch (err) {
+    console.log(err);
+    return [];
+    }
+
+    console.log(">>> Delete user complete");
+}
+
+export { handleCreateUser, getAllUsers, handleDeleteUser };
